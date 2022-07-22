9879dcc058ffb707fb6380b6ddbd0bb58875c1f4
Orlandoj77 <laudesjones@gmail.com>
2022-07-22 19:39:57 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -65,25 +65,25 @@ class App extends Component {
   evaluate = () => {
     this.state.currentNumber = this.state.input;
 
-    if (this.state.operator == "plus") {
+    if (this.state.operator === "plus") {
       this.setState({
         input:
           parseInt(this.state.previousNumber) +
           parseInt(this.state.currentNumber),
       });
-    } else if (this.state.operator == "subtract") {
+    } else if (this.state.operator === "subtract") {
       this.setState({
         input:
           parseInt(this.state.previousNumber) -
           parseInt(this.state.currentNumber),
       });
-    } else if (this.state.operator == "multiply") {
+    } else if (this.state.operator === "multiply") {
       this.setState({
         input:
           parseInt(this.state.previousNumber) *
           parseInt(this.state.currentNumber),
       });
-    } else if (this.state.operator == "divide") {
+    } else if (this.state.operator === "divide") {
       this.setState({
         input:
           parseInt(this.state.previousNumber) /
